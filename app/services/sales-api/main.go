cf7ff9f058365e61d27a5c1dc602052fe6a9bfee
rmishgoog <rohitmishra@google.com>
2024-04-28 12:27:45 -0500

    first commit after adding the logger

---
+++ app/services/sales-api/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"context"
+	"os"
+	"os/signal"
+	"runtime"
+	"syscall"
+
+	"github.com/rmishgoog/starter-go-service/foundations/logger"
+)
+
+func main() {
+
+	var log *logger.Logger
+
+	events := logger.Events{
+		Error: func(ctx context.Context, r logger.Record) {
+			log.Info(ctx, "***** SENDING ALERT *****")
+		},
+	}
+
+	traceIDFunc := func(ctx context.Context) string {
+		return "123456789"
+
+	}
+
+	log = logger.NewWithEvents(os.Stdout, logger.LevelInfo, "SALES-API", traceIDFunc, events)
+
+	ctx := context.Background()
+
+	if err := run(ctx, log); err != nil {
+		log.Error(ctx, "startup", "msg", err)
+		return
+	}
+}
+
+func run(ctx context.Context, log *logger.Logger) error {
+
+	log.Info(ctx, "startup", "GOMAXPROCS", runtime.GOMAXPROCS(0))
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, syscall.SIGINT, syscall.SIGTERM)
+	sig := <-shutdown
+	log.Info(ctx, "shutdown", "status", "shutdown started", "signal", sig)
+	defer log.Info(ctx, "shutdown", "status", "shutdown complete", "signal", sig)
+
+	return nil
+
+}
