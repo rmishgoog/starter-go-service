245a2bf71668be421373ad9ec018ec86c7fdb60e
rmishgoog <rohitmishra@google.com>
2024-06-01 19:24:28 -0500

    starting the error handling

--- foundations/web/web.go
+++ foundations/web/web.go
@@ -2,9 +2,10 @@ package web
 
 import (
 	"context"
-	"fmt"
+	"errors"
 	"net/http"
 	"os"
+	"syscall"
 	"time"
 
 	"github.com/dimfeld/httptreemux/v5"
@@ -35,6 +36,10 @@ func NewApp(shutdown chan os.Signal, mw ...MiddleWare) *App {
 	}
 }
 
+func (a *App) SignalShutdown() {
+	a.shutdown <- syscall.SIGTERM
+}
+
 type Handler func(ctx context.Context, w http.ResponseWriter, r *http.Request) error
 
 // Handle sets a handler function for a given HTTP method and path pair
@@ -52,9 +57,49 @@ func (a *App) Handle(method string, path string, handler Handler, mw ...MiddleWa
 
 		ctx := setValues(r.Context(), &v)
 		if err := handler(ctx, w, r); err != nil {
-			fmt.Println(err)
-			return
+			if validateError(err) {
+				a.SignalShutdown()
+				return
+			}
 		}
 	}
 	a.ContextMux.Handle(method, path, h)
+}
+
+func validateError(err error) bool {
+
+	// Ignore syscall.EPIPE and syscall.ECONNRESET errors which occurs
+	// when a write operation happens on the http.ResponseWriter that
+	// has simultaneously been disconnected by the client (TCP
+	// connections is broken). For instance, when large amounts of
+	// data is being written or streamed to the client.
+	// https://blog.cloudflare.com/the-complete-guide-to-golang-net-http-timeouts/
+	// https://gosamples.dev/broken-pipe/
+	// https://gosamples.dev/connection-reset-by-peer/
+
+	switch {
+	case errors.Is(err, syscall.EPIPE):
+
+		// Usually, you get the broken pipe error when you write to the connection after the
+		// RST (TCP RST Flag) is sent.
+		// The broken pipe is a TCP/IP error occurring when you write to a stream where the
+		// other end (the peer) has closed the underlying connection. The first write to the
+		// closed connection causes the peer to reply with an RST packet indicating that the
+		// connection should be terminated immediately. The second write to the socket that
+		// has already received the RST causes the broken pipe error.
+		return false
+
+	case errors.Is(err, syscall.ECONNRESET):
+
+		// Usually, you get connection reset by peer error when you read from the
+		// connection after the RST (TCP RST Flag) is sent.
+		// The connection reset by peer is a TCP/IP error that occurs when the other end (peer)
+		// has unexpectedly closed the connection. It happens when you send a packet from your
+		// end, but the other end crashes and forcibly closes the connection with the RST
+		// packet instead of the TCP FIN, which is used to close a connection under normal
+		// circumstances.
+		return false
+	}
+
+	return true
 }
